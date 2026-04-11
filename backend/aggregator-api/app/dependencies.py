b3e692847965cd1949e7e99fdeb203a5dcbca769
Darren Batarina <batarina.darren@gmail.com>
2026-04-11 21:27:29 +1000

    remove loading env, instead access from centralised config

--- backend/aggregator-api/app/dependencies.py
+++ backend/aggregator-api/app/dependencies.py
@@ -1,17 +1,14 @@
 from typing import Annotated
-import os
-from dotenv import load_dotenv
 from fastapi import Security, HTTPException
 from fastapi.security.api_key import APIKeyHeader
+from .config.config import environment 
 
-# Load environment variables
-load_dotenv()
-
-API_KEY = os.getenv("API_KEY")
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 async def verify_api_key(api_key: str = Security(api_key_header)):
 
     # if api key missing, or does not match 
-    if not api_key or api_key != API_KEY:
+    if not api_key or api_key != environment.api_key:
         raise HTTPException(status_code=401, detail="Invalid or missing API key")
+    
+    return True
